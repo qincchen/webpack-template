368a749041a7a3470d3554e3ce0b0fc324ddc32d
qin cheng chen <qincchen@gmail.com>
2017-02-09 20:13:02 -0500

    re-enable react hot reload

--- app/index.js
+++ app/index.js
@@ -5,25 +5,25 @@ import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 
 require.ensure(['./component'], () => {
-
-  const { TestComponent } = require('./component');
-
-  const render = (Component) => {
-    ReactDOM.render(
-      <AppContainer>
-        <Component/>
-      </AppContainer>,
-      document.getElementById('root')
-    );
-  };
-
-  render(TestComponent);
-
-  // Hot Module Replacement API
-  if (module.hot) {
-    module.hot.accept('./component', () => {
-      render(TestComponent);
-    });
-  }
-
+// need conditional for production build
 }, 'component');
+
+const { TestComponent } = require('./component');
+
+const render = (Component) => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component/>
+    </AppContainer>,
+    document.getElementById('root')
+  );
+};
+
+render(TestComponent);
+
+// Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept('./component', () => {
+    render(TestComponent);
+  });
+}
